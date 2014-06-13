9ce98817869c33e4c938141678c75ee79bdfa710
Gaurav Chande <gmail@gauravchande.com>
2014-06-13 17:29:55 -0400

    Bracketify

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Shopifify::Application.config.google_client_id, Shopifify::Application.config.google_client_secret
+  provider :google_oauth2, Bracketify::Application.config.google_client_id, Bracketify::Application.config.google_client_secret
 end
